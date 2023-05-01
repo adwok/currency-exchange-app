62fa4023afd6545cef5ec38771dee60a72f1f90f
Adam Chan <adwok@Adams-MacBook-Air.local>
2023-05-01 15:47:36 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -40,57 +40,67 @@ function CurrencyMenu(props) {
       </select>
     </div>
   );
-}
-
-//function FirstSelection() {
-//  return (
-//    <CurrencyMenu />
-//  )
-//}
-
-//function SecondSelection() {
-//  return (
-//    <CurrencyMenu />
-//  )
-//}
-
-//function MyButton() {
-//  return (
-//    <button>
-//      Reverse
-//    </button>
-//  );
-//}
+};
 
 class MyApp extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      first_currency: 'USD',
-      second_currency: 'EUR',
+      firstCurrency: "AUD",
+      secondCurrency: "AUD",
+      rates: { AUD: 1 },
+      firstAmount: "",
+      secondAmount: "",
     };
-  }
+  };
 
+  GetRate = () => {
+    fetch (`https://api.frankfurter.app/latest?from=${this.state.firstCurrency}&to=${this.state.secondCurrency}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ rates: data.rates });
+    });
+  };
 
   handleFirstCurrChange = (event) => {
-    this.setState({ first_currency: event.target.value });
-  }
+    this.setState({ firstCurrency: event.target.value }, this.GetRate);
+    this.setState({ firstAmount: 1 });
+  };
 
   handleSecondCurrChange = (event) => {
-    this.setState({ second_currency: event.target.value });
-  }
+    this.setState({ secondCurrency: event.target.value }, this.GetRate);
+    this.setState({ firstAmount: 1 });
+  };
+
+  handleFirstAmountChange = (event) => {};
+
+  handleSecondAmountChange = (event) => {};
 
   render () {
     return (
       <div className="text-center">
         <h1>CURRENCY EXCHANGE APP</h1>
         <p>Exchange between</p>
-        <CurrencyMenu onChange={this.handleFirstCurrChange} value={this.state.first_currency} />
-        <p>and</p>
-        <CurrencyMenu onChange={this.handleSecondCurrChange} value={this.state.second_currency} />
+        <div className="col-12">
+          <CurrencyMenu onChange={this.handleFirstCurrChange} value={this.state.firstCurrency} />
+          <span className="mx-3">and</span>
+          <CurrencyMenu onChange={this.handleSecondCurrChange} value={this.state.secondCurrency} />
+        </div>
+        <div className="col-12">
+          <span className="mx-3">1 {this.state.firstCurrency} equals {this.rates[this.secondCurrency]} {this.state.secondCurrency}</span>
+        </div>
+        <div className="col-12">
+          <span className="mr-1">{this.state.firstCurrency}</span>
+          <input value={this.state.firstAmount} onChange={this.handleFirstAmountChange} type="number"/>
+          <span className="mx-3">=</span>
+          <input value={this.state.secondAmount} onChange={this.handleSecondAmountChange} type="number"/>
+          <span className="ml-1">{this.state.secondCurrency}</span>
+        </div>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default MyApp;
